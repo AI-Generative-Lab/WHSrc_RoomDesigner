5232016ad5959573354e4284436023eaa01066cc
QuantumReaperBot <125912415+QuantumReaperBot@users.noreply.github.com>
2023-06-02 22:20:40 +0600

    Add files via upload

---
+++ WriteHomes/config/locale.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'en' => [
+        'code' => 'en',
+        'display' => 'English',
+        'flag' => 'us'
+    ],
+    'jp' => [
+        'code' => 'JP',
+        'display' => 'Japanese',
+        'flag' => 'jp'
+    ], 
+];
